f89ebef375dc315ef8941f9bb94beeee4d06e3b7
Lucas Sens <lucassousasens@gmail.com>
2024-10-02 21:27:54 -0300

    updade

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,21 @@
-// Importar os plugins
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
-const terser = require('gulp-terser');
 const concat = require('gulp-concat');
 
-// Caminhos dos arquivos
-const paths = {
-  styles: {
-    src: 'src/scss/**/*.scss',
-    dest: 'dist/css/'
-  },
-  scripts: {
-    src: 'src/js/**/*.js',
-    dest: 'dist/js/'
-  }
-};
-
-// Compilar SCSS em CSS e minificar
-function styles() {
-  return gulp.src(paths.styles.src)
+// Compilar Sass e mover para src/dist/css
+gulp.task('styles', function() {
+  return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
-    .pipe(gulp.dest(paths.styles.dest));
-}
+    .pipe(gulp.dest('src/dist/css'));  // Destino dentro de src
+});
 
-// Minificar e concatenar JavaScript
-function scripts() {
-  return gulp.src(paths.scripts.src)
+// Minificar e concatenar JS e mover para src/dist/js
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
     .pipe(concat('main.js'))
-    .pipe(terser()) // Minificador de ES6+
-    .pipe(gulp.dest(paths.scripts.dest));
-}
-
-// Vigiar alterações em arquivos SCSS e JS
-function watch() {
-  gulp.watch(paths.styles.src, styles);
-  gulp.watch(paths.scripts.src, scripts);
-}
-
-// Exportar as tarefas
-exports.styles = styles;
-exports.scripts = scripts;
-exports.watch = watch;
+    .pipe(uglify())
+    .pipe(gulp.dest('src/dist/js'));  // Destino dentro de src
+});
